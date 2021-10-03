24c3e7f1a132525a1714f04653aa5a810ec0cd09
Yancheeto <yanni.stoyanova@gmail.com>
2021-10-03 12:01:51 -0400

    Added second file, updated the first

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,15 +3,14 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello Sunday");
-
         // this is a new line, with a comment about the file
-
         System.out.println("Another week of training in the books");
 
         //text doesn't matter here
 
         System.out.println("October is beautiful month");
 
-        //another random text
+
+
     }
 }
