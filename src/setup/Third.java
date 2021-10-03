6cd93be5cd24125ffec78fea2e8b5369071941bb
Yancheeto <yanni.stoyanova@gmail.com>
2021-10-03 12:44:17 -0400

    Updates on Third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,6 +4,9 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("Printing for life");
         System.out.println("Looking up tea options");
+        //these comments are useless
+        //anyways keep making them
+
     }
 
 }
