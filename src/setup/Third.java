6b9c507a2cac297844e827d60d55d4d85e7732d5
Yancheeto <yanni.stoyanova@gmail.com>
2021-10-03 12:43:24 -0400

    Created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,9 @@
+package setup;
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("Printing for life");
+        System.out.println("Looking up tea options");
+    }
+
+}
