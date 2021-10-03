c4832edacc3716a8dff3fb5b736125cdf03f3eaa
Yancheeto <yanni.stoyanova@gmail.com>
2021-10-03 11:31:01 -0400

    Updated first file

--- src/setup/First.java
+++ src/setup/First.java
@@ -4,6 +4,10 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello Sunday");
 
+        // this is a new line, with a comment about the file
 
+        System.out.println("Another week of training in the books");
+
+        //text doesn't matter here
     }
 }
